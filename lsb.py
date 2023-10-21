c34835e18b228154ed7d0ceb5c8d6a6a2c9edfa3
KacBounce <kacperb223@wp.pl>
2023-10-21 17:45:39 +0200

    Final commit for optimization

---
+++ lsb.py
@@ -0,0 +1,391 @@
+import numpy as np
+from PIL import Image
+import random
+import cv2
+
+delimiter = '##END'
+delimiter = ''.join(format(ord(char), '08b') for char in delimiter)
+image = cv2.imread('lenna(test_image).png', 0)
+
+
+
+secret_image = Image.open('Secret Lenna.png', 'r')
+secret_width, secret_height = secret_image.size
+secret_image = secret_image.convert('L')
+
+def create_chromosome():        
+    chromosome = random.randint(0,2097152)
+    binary_chromosome = format(chromosome,'b')
+    while(len(binary_chromosome)<21):
+        binary_chromosome = "0" + binary_chromosome
+    return binary_chromosome
+
+
+def transform_secret_image(secret_image, binary_chromosome):
+    binary_data = ''.join(format(pixel, '08b') for pixel in secret_image.tobytes())
+    if(binary_chromosome[1] == '1'):
+        binary_data = ''.join('1' if bit == '0' else '0' for bit in binary_data)
+    if(binary_chromosome[0] == '1'):
+        binary_data = binary_data[::-1]
+    return binary_data
+
+def transform_bits_image(binary_data, width, height, binary_chromosome):
+    mode = 'L'  # Grayscale mode 
+
+    # Create an empty image
+    secret_image = Image.new(mode, (width, height))
+    
+    if(binary_chromosome[1] == '1'):
+        binary_data = ''.join('1' if bit == '0' else '0' for bit in binary_data)
+    if(binary_chromosome[0] == '1'):
+        binary_data = binary_data[::-1]
+
+    # Parse the list of bits and set pixel values
+    pixels = [int(binary_data[i:i+8], 2) for i in range(0, len(binary_data), 8)]
+    secret_image.putdata(pixels)
+    return secret_image
+
+
+def hide_bit(value, bit):
+    bin_pixel_str = format(value,'b')
+    bin_pixel = list(bin_pixel_str)
+    if(bin_pixel[len(bin_pixel) - 1] != bit):
+        bin_pixel[len(bin_pixel) - 1] = bit
+    bin_pixel_str = "".join(bin_pixel)
+    return int(bin_pixel_str, 2)
+    
+
+def Encode(host, binary_message, binary_chromosome):
+    global delimiter
+    height, width = host.shape
+    transformed = host.copy()
+    index = 0
+    index2 = 0
+    #first direction
+    if (binary_chromosome[18:21]== "000"):
+        for h in range(int(binary_chromosome[2:10], 2), height):
+            for w in range(int(binary_chromosome[10:18], 2), width):                       
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #second direction
+    elif (binary_chromosome[18:21]== "001"):
+        for w in range(int(binary_chromosome[10:18], 2), width):
+            for h in range(int(binary_chromosome[2:10], 2), height):               
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #third direction
+    elif (binary_chromosome[18:21]== "010"):
+        for h in range(int(binary_chromosome[2:10], 2), height):
+            for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):            
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #fourth direction
+    elif (binary_chromosome[18:21]== "011"):
+        for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):
+            for h in range(int(binary_chromosome[2:10], 2), height):             
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #fifth direction
+    elif (binary_chromosome[18:21]== "100"): 
+         for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):
+            for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):             
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #sixth direction
+    elif (binary_chromosome[18:21]== "101"):
+        for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):
+            for w in range(width- 1 - int(binary_chromosome[10:18], 2), 0, -1):              
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #seventh direction
+    elif (binary_chromosome[18:21]== "110"):
+        for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1): 
+            for w in range(int(binary_chromosome[10:18], 2), width):
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    #eigth direction
+    elif (binary_chromosome[18:21]== "111"):
+        for w in range(int(binary_chromosome[10:18], 2), width):
+            for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):        
+                if (index < len(binary_message)):
+                    transformed[h][w] = hide_bit(transformed[h][w], binary_message[index])
+                    index += 1
+                elif (index < len(binary_message) + len(delimiter)):
+                    if(index2 < len(delimiter)):
+                        transformed[h][w] = hide_bit(transformed[h][w], delimiter[index2])
+                        index2 += 1
+    return transformed                    
+
+def Decode(host, binary_chromosome):
+    global delimiter
+    height, width = host.shape
+    binary_data = ""
+    #first direction
+    if (binary_chromosome[18:21]== "000"):
+        for h in range(int(binary_chromosome[2:10], 2), height):
+            for w in range(int(binary_chromosome[10:18], 2), width): 
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #second direction
+    elif (binary_chromosome[18:21]== "001"):
+        for w in range(int(binary_chromosome[10:18], 2), width):
+            for h in range(int(binary_chromosome[2:10], 2), height):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #third direction
+    elif (binary_chromosome[18:21]== "010"):
+        for h in range(int(binary_chromosome[2:10], 2), height):
+            for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #fourth direction
+    elif (binary_chromosome[18:21]== "011"):
+        for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):
+            for h in range(int(binary_chromosome[2:10], 2), height):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #fifth direction
+    elif (binary_chromosome[18:21]== "100"):
+        for w in range(width - 1 - int(binary_chromosome[10:18], 2), 0, -1):
+            for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #sixth direction
+    elif (binary_chromosome[18:21]== "101"):
+        for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):
+            for w in range(width- 1 - int(binary_chromosome[10:18], 2), 0, -1):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #seventh direction
+    elif (binary_chromosome[18:21]== "110"):
+        for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1): 
+            for w in range(int(binary_chromosome[10:18], 2), width):
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    #eigth direction
+    elif (binary_chromosome[18:21]== "111"):
+        for w in range(int(binary_chromosome[10:18], 2), width):
+            for h in range(height - 1 - int(binary_chromosome[2:10], 2), 0, -1):  
+                if(binary_data[-40:] != delimiter):
+                    bin_pixel_str = format(host[h][w],'b')
+                    bin_pixel = list(bin_pixel_str)
+                    binary_data += bin_pixel[len(bin_pixel) - 1]
+                else:
+                    break
+    
+    return binary_data[:-40]
+
+#TEST
+
+# binary_chromosome = create_chromosome()
+# image = cv2.imread('Lenna(test_image).png', 0)
+# #secret_image = Image.open('Lenna(test_image).png', 'r')
+# secret_image = Image.open('Secret Lenna.png', 'r')
+# array = np.array(list(secret_image.getdata()))
+# secret_image = secret_image.convert('L')
+# binary_data = transform_secret_image(secret_image, binary_chromosome)
+
+# lol = Encode(image, binary_data, binary_chromosome)
+# cv2.imshow('Hidden',lol)
+# diff = image - lol
+# print(cv2.PSNR(image, image))
+# recovered_data = Decode(lol,  binary_chromosome)
+
+# secret_image_back = transform_bits_image(recovered_data, 100, 100, binary_chromosome)
+# secret_image_back.save('reconstructed_image.png')
+# secret_image2 = cv2.imread('reconstructed_image.png')
+# cv2.imshow('Recovered', secret_image2) 
+
+
+def fitness_function(binary_chromosome):
+    global image, secret_image
+    binary_data = transform_secret_image(secret_image, binary_chromosome)
+    secret = Encode(image, binary_data, binary_chromosome)
+    return cv2.PSNR(image, secret) 
+
+
+def genetic():      
+    # Genetic Algorithm parameters
+    population_size = 50
+    num_generations = 10
+    mutation_rate = 0.1
+
+    # Initialization: Create an initial population
+    population = [create_chromosome() for _ in range(population_size)]
+
+    for generation in range(num_generations):
+        # Fitness Evaluation
+        fitness_scores = [fitness_function(x) for x in population]
+        
+        # Selection: Roulette wheel selection
+        selected_parents = random.choices(population, weights=fitness_scores, k=population_size)
+        
+        # Crossover: One-point crossover
+        offspring = []
+        for i in range(0, population_size, 2):
+            parent1 = selected_parents[i]
+            parent2 = selected_parents[i + 1]
+            crossover_point = random.randint(1, 21 - 1)
+            offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
+            offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
+            offspring.extend([offspring1, offspring2])
+
+        # Mutation: Flip individual bits with a low probability
+        for i in range(population_size):
+            for j in range(21):
+                if random.random() < mutation_rate:
+                    offspring[i] = offspring[i][:j] + ("0" if offspring[i][j] == "1" else "1") + offspring[i][j+1:]
+
+        # Replacement: Replace the old population with the offspring
+        population = offspring
+
+    # Print the best solution found
+    best_solution = max(population, key=fitness_function)
+    print("Best solution:", best_solution, "PSNR : ", fitness_function(best_solution))
+    binary_data = transform_secret_image(secret_image, best_solution)
+    print(len(binary_data))
+    lol = Encode(image, binary_data, best_solution)
+    cv2.imshow('Hidden',lol)
+    cv2.imwrite('Hidden_lsb.png', lol)
+    diff = image - lol
+
+    cv2.imshow('Difference', diff)
+    recovered_data = Decode(lol, best_solution)
+
+    if(recovered_data == binary_data):
+        print('success')
+        secret_image_back = transform_bits_image(recovered_data, secret_width, secret_height, best_solution)
+        secret_image_back.save('reconstructed_image.png')
+        secret_image2 = cv2.imread('reconstructed_image.png')
+        cv2.imshow('Recovered', secret_image2)
+    cv2.waitKey(0) 
+
+def pso():
+    global secret_image
+    # Define the problem parameters
+    num_particles = 30
+    num_dimensions = 21  # Length of the binary chromosome
+    max_iterations = 100
+    c1 = 2.0  # Cognitive coefficient
+    c2 = 2.0  # Social coefficient
+    # Initialize the particles' positions and velocities
+    best_positions = []  # Best positions for each particle
+    global_best_position = None  # Global best position
+    
+    particles = []
+    for _ in range(num_particles):
+        particle = [random.choice([0, 1]) for _ in range(num_dimensions)]
+        particles.append(particle)
+        best_positions.append(list(particle))
+        
+    velocities = []
+    for _ in range(num_particles):
+        velocity = [random.uniform(-1, 1) for _ in range(num_dimensions)]
+        velocities.append(velocity)
+    # PSO Loop
+    for iteration in range(max_iterations):
+        for i in range(num_particles):
+            # Evaluate the fitness of the current particle
+            fitness = fitness_function(particles[i])
+
+            # Update the best-known position for the particle
+            if fitness_function(best_positions[i]) < fitness:
+                best_positions[i] = list(particles[i])
+
+            # Update the global best position
+            if global_best_position is None or fitness_function(global_best_position) < fitness:
+                global_best_position = list(particles[i])
+
+            # Update the particle's velocity and position
+            for j in range(num_dimensions):
+                r1, r2 = random.random(), random.random()
+                cognitive_component = c1 * r1 * (best_positions[i][j] - particles[i][j])
+                social_component = c2 * r2 * (global_best_position[j] - particles[i][j])
+                velocities[i][j] = velocities[i][j] + cognitive_component + social_component
+                # Update the particle's position (using binary update)
+                particles[i][j] = 1 if random.random() < 1 / (1 + 2.7**(-velocities[i][j])) else 0
+    best_particle = ["1" if x == 1 else "0" for x in global_best_position]
+    best_particle = ''.join(best_particle)
+    print("Best solution:", best_particle, "PSNR : ", fitness_function(best_particle))
+    binary_data = transform_secret_image(secret_image, best_particle)
+    print(len(binary_data))
+    lol = Encode(image, binary_data, best_particle)
+    cv2.imshow('Hidden',lol)
+    cv2.imwrite('Hidden_lsb.png', lol)
+    diff = image - lol
+
+    cv2.imshow('Difference', diff)
+    recovered_data = Decode(lol, best_particle)
+
+    if(recovered_data == binary_data):
+        print('success')
+        secret_image_back = transform_bits_image(recovered_data, secret_width, secret_height, global_best_position)
+        secret_image_back.save('reconstructed_image.png')
+        secret_image2 = cv2.imread('reconstructed_image.png')
+        cv2.imshow('Recovered', secret_image2)
+    cv2.waitKey(0) 
+pso()
+        
--- psnr.py
+++ psnr.py
@@ -9,7 +9,7 @@ local_max = 35
 local_curr_sets = []
 local_set_indexes = []
 
-img1 = cv2.imread('C:\\Users\\kbednarski\\OneDrive - Anegis\\Desktop\\Inzynierka\\Lenna(test_image).png', cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread('Lenna(test_image).png', cv2.IMREAD_GRAYSCALE)
 lock = threading.Lock()
 
 def get_image_from_blocks(blocks):
@@ -89,15 +89,10 @@ def hide(set_array, binary_array, msg_length_bin):
                     counter = counter + 1
     return set_array
 
-def pso():
+def psnr():
     global local_max, local_curr_sets, local_set_indexes, lock
-    message = "No hej kurczaki"
-    binary_message = ''.join(format(ord(char), '08b') for char in message)
-    binary_array = [int(bit) for bit in binary_message]
-    msg_length = len(binary_array)
-    msg_length_bin = [int(bit) for bit in bin(msg_length)[2:]]   
 
-    max_iterations = 100
+    max_iterations = 10
     for i in range(max_iterations): 
         blocks = get_blocks(img1, 0)  
         sets = get_sets_from_blocks(blocks)
@@ -106,10 +101,50 @@ def pso():
             for j in i:
                 set_array.append(i[j]) 
 
+        
+        #dct_image_with_info = cv2.dct(np.float32(image_with_info))
+
+        #cv2.imshow('hidden', image_with_info)
+        #cv2.imwrite('hidden.jpg', image_with_info)
+
+        #dct_image = cv2.dct(np.float32(img1))
+        #cv2.imshow('dct normal', dct_image)  
+        #cv2.imshow('dct hidden', dct_image_with_info)
+
+       # cv2.waitKey(0)
+
+       # cv2.destroyAllWindows()
+
+        psnr = cv2.PSNR(img1, image_with_info)
+
+        if (psnr > local_max):
+            lock.acquire()
+            local_max = psnr
+            local_curr_sets = curr_sets
+            local_set_indexes = set_indexes
+            lock.release()
+
+
+# Particle Swarm Optimization (PSO) function
+def pso(cost_func, dim=2, num_particles=30, max_iter=100, w=0.5, c1=1, c2=2):
+    # Initialize particles and velocities
+    message = "No hej kurczaki"
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    binary_array = [int(bit) for bit in binary_message]
+    msg_length = len(binary_array)
+    msg_length_bin = [int(bit) for bit in bin(msg_length)[2:]]   
+    particles = []
+    blocks = get_blocks(img1, 0)  
+    sets = get_sets_from_blocks(blocks)
+    set_array = []
+    for i in sets:
+        for j in i:
+            set_array.append(i[j]) 
+    for _ in range(num_particles):
         curr_sets = []
         set_indexes = []
         for i in range(msg_length + max_message_bits):
-            index = random.randint(0, len(set_array))   
+            index = random.randint(0, len(set_array))
             curr_sets.append(set_array[index])
             set_indexes.append(index)
         curr_sets = hide(curr_sets, binary_array, msg_length_bin)
@@ -166,41 +201,68 @@ def pso():
 
         dequantized_blocks = dequantize_blocks(blocks)
         image_with_info = get_image_from_blocks(dequantized_blocks)
-        #dct_image_with_info = cv2.dct(np.float32(image_with_info))
+        particles.append(image_with_info)
 
-        #cv2.imshow('hidden', image_with_info)
-        #cv2.imwrite('hidden.jpg', image_with_info)
+    # Initialize velocities
+    velocities = np.zeros((num_particles, dim))
 
-        #dct_image = cv2.dct(np.float32(img1))
-        #cv2.imshow('dct normal', dct_image)  
-        #cv2.imshow('dct hidden', dct_image_with_info)
+    # Initialize the best positions and fitness values
+    best_positions = particles.copy()
+    best_fitness = np.array([cost_func(p) for p in particles])
+    swarm_best_position = best_positions[np.argmin(best_fitness)]
+    swarm_best_fitness = np.min(best_fitness)
 
-       # cv2.waitKey(0)
+    # Iterate through the specified number of iterations, updating the velocity and position of each particle at each iteration
+    for i in range(max_iter):
+        # Update velocities
+        r1 = np.random.uniform(0, 1, (num_particles, dim))
+        r2 = np.random.uniform(0, 1, (num_particles, dim))
+        for j in range(num_particles):
+            velocities[j] = w * velocities[j] + c1 * r1[j] * (np.array(best_positions[j]) - np.array(particles[j])) + c2 * r2[j] * (np.array(swarm_best_position) - np.array(particles[j]))
 
-       # cv2.destroyAllWindows()
+        # Update positions
+        for j in range(num_particles):
+            particles[j] = particles[j] + velocities[j]
 
-        psnr = cv2.PSNR(img1, image_with_info)
+        # Evaluate fitness of each particle
+        fitness_values = np.array([cost_func(p) for p in particles])
 
-        if (psnr > local_max):
-            lock.acquire()
-            local_max = psnr
-            local_curr_sets = curr_sets
-            local_set_indexes = set_indexes
-            lock.release()
+        # Update best positions and fitness values
+        for j in range(num_particles):
+            if fitness_values[j] < best_fitness[j]:
+                best_positions[j] = particles[j]
+                best_fitness[j] = fitness_values[j]
 
+        # Update swarm best position and fitness
+        if np.min(fitness_values) < swarm_best_fitness:
+            swarm_best_position = particles[np.argmin(fitness_values)]
+            swarm_best_fitness = np.min(fitness_values)
 
+    # Return the best solution found by the PSO algorithm
+    return swarm_best_position, swarm_best_fitness
 
-t1 = threading.Thread(target=pso)
-t2 = threading.Thread(target=pso)
-t3 = threading.Thread(target=pso)
-t4 = threading.Thread(target=pso)
+# Cost function for PSO (PSNR calculation)
+def psnr_cost(x):
+    psnr_value = cv2.PSNR(img1, x)
 
-t1.start()
-t2.start()
-t3.start()
-t4.start()
+    return psnr_value
 
-t1.join()
-t2.join()
-t3.join()
-t4.join()
+# Main PSO execution
+max_iterations = 10
+best_psnr = 0.0
+best_solution = []
+
+for _ in range(max_iterations):
+    # Call PSO to find the best set indexes
+    best_set_indexes, best_psnr = pso(psnr_cost, dim=max_message_bits, num_particles=30, max_iter=100)
+    
+    # Update the best solution if a better one is found
+    if best_psnr > local_max:
+        lock.acquire()
+        local_max = best_psnr
+        local_set_indexes = best_set_indexes
+        lock.release()
+
+# Print the best PSNR and corresponding set indexes
+print("Best PSNR:", local_max)
+print("Best Set Indexes:", local_set_indexes)
